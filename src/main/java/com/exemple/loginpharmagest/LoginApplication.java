4e27335e290deb01e3705dd1c40c7ff0b519602b
Omar.py <142530504+anliomar-dev@users.noreply.github.com>
2024-05-16 19:32:07 +0400

    Update LoginApplication.java
    DATABASE

--- src/main/java/com/exemple/loginpharmagest/LoginApplication.java
+++ src/main/java/com/exemple/loginpharmagest/LoginApplication.java
@@ -21,3 +21,24 @@ public class LoginApplication extends Application {
         launch();
     }
 }
+package com.example.pharmagest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DATABASE_NAME = "phramagest";
+    private static final String DATABASE_USER = "postgres";
+    private static final String DATABASE_PASSWORD = "@Anliomar285.";
+    private static final String URL = "jdbc:postgresql://localhost:5432/" + DATABASE_NAME;
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, DATABASE_USER, DATABASE_PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Database driver not found", e);
+        }
+    }
+}
